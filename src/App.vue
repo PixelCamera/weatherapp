53efc38b2165bb64a6b12078fb019b572c1859ce
Chenliang Yan <chenliang.yan@yundx.com>
2023-09-26 23:47:53 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <RouterView />
+  </div>
+</template>
