2c1e2ae67e4d60c9b4243b80c10dd3bbd1c9777b
Chenliang Yan <chenliang.yan@yundx.com>
2023-09-28 01:00:52 +0800

    feat(component): add new basic model page

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Spacer from "@/components/global/Spacer.vue";
+import HStack from "@/components/global/HStack.vue";
+import VStack from "@/components/global/VStack.vue";
+
+const closeButtonText = "关闭"; // 按钮文字
+</script>
+<template>
+  <!--Mask-->
+  <div
+    class="absolute left-0 top-0 flex h-screen w-full justify-center bg-black bg-opacity-30 px-8"
+  >
+    <!--Modal-->
+    <v-stack class="m-32 max-w-screen-md self-start rounded bg-white p-10">
+      <!--Slot-->
+      <slot />
+
+      <!--Footer-->
+      <h-stack class="w-full gap-2">
+        <Spacer />
+        <button
+          class="rounded bg-weather-primary px-6 py-2 text-white hover:bg-weather-secondary"
+        >
+          {{ closeButtonText }}
+        </button>
+      </h-stack>
+    </v-stack>
+  </div>
+</template>
--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -27,6 +27,40 @@ const appName = "本地天气";
       <i
         class="fa-solid fa-circle-info duration-150 hover:text-weather-secondary"
       ></i>
-    </HStack>
-  </HStack>
+      <!--BaseModal-->
+      <base-modal>
+        <!--Modal Content-->
+        <div class="flex flex-col gap-2 text-black">
+          <h1
+            class="mb-4 text-2xl font-bold underline decoration-weather-primary decoration-4 underline-offset-8"
+          >
+            关于
+          </h1>
+          <p class="mb-4">“本地天气”允许您跟踪您选择的城市的当前和未来天气。</p>
+          <h-stack class="gap-2"
+            ><i
+              class="fa-solid fa-circle-question fa-beat w-6 text-2xl text-weather-primary"
+            ></i>
+            <h2 class="text-xl font-bold">如何使用</h2></h-stack
+          >
+          <ol class="mb-4 list-inside list-decimal">
+            <li>通过在搜索栏中输入名称来搜索您的城市。</li>
+            <li>在结果中选择一个城市，这将带您查看所选城市的当前天气。</li>
+            <li>
+              通过点击右上角的“+”图标来跟踪该城市。这将保存该城市，以便稍后在主页上查看。
+            </li>
+          </ol>
+
+          <h-stack class="gap-2"
+            ><i class="fa-solid fa-trash fa-beat w-6 text-2xl text-red-600"></i>
+            <h2 class="text-xl font-bold">删除城市</h2></h-stack
+          >
+          <p class="mb-4">
+            如果您不再希望跟踪某个城市，只需在主页上选择该城市。
+            在页面底部，将有一个删除城市的选项。
+          </p>
+        </div>
+      </base-modal>
+    </h-stack>
+  </h-stack>
 </template>
