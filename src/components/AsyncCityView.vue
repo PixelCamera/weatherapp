ef5777725c687fc5578010a416dc9ddda367e274
Chenliang Yan <chenliang.yan@yundx.com>
2023-10-01 02:50:20 +0800

    feat(CityView): Add Weather Detail

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,43 +1,220 @@
 <script setup>
 import axios from "axios";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { useRoute } from "vue-router";
+import dayjs from "dayjs";
+import "dayjs/locale/zh-cn";
+import HStack from "@/components/global/HStack.vue";
+import VStack from "@/components/global/VStack.vue";
+import WeatherDetailCard from "@/components/WeatherDetailCard.vue"; // 导入中文语言包
+dayjs.locale("zh-cn"); // 使用中文语言包
 
 const route = useRoute(); // 获取路由实例
-const weatherData = ref(null); // 存储天气数据
-const API_KEY = "7d0835ccc03346ffb8dc8e5525272f98"; // 和风天气 API 的密钥
 
-// 获取天气数据
-const getWeatherData = async () => {
+// 和风天气 API 的密钥
+const API_KEY = "7d0835ccc03346ffb8dc8e5525272f98";
+
+// 存储天气数据
+const currentWeather = ref(); //实时天气预报
+const dailyForecast = ref(null); // 每日天气预报
+const hourlyForecast = ref(null); // 每小时天气预报
+
+// 格式化更新时间
+const updateTimeFormatted = dayjs(currentWeather.updateTime).format(
+  "YYYY-MM-DD HH:mm dddd",
+);
+
+// 定义天气详情卡片的数据，包括图标、标签和值
+
+const weatherItems = computed(() => {
+  if (!currentWeather.value) {
+    return [];
+  }
+  return [
+    {
+      icon: "fas fa-wind",
+      label: "风向",
+      value: currentWeather.value.now.windDir,
+    },
+    {
+      icon: "fas fa-compass",
+      label: "风向角度",
+      value: currentWeather.value.now.wind360 + "°",
+    },
+    {
+      icon: "fas fa-fan",
+      label: "风力等级",
+      value: currentWeather.value.now.windScale,
+    },
+    {
+      icon: "fas fa-wind",
+      label: "风速",
+      value: currentWeather.value.now.windSpeed + "km/h",
+    },
+    {
+      icon: "fas fa-tint",
+      label: "湿度",
+      value: currentWeather.value.now.humidity + "%",
+    },
+    {
+      icon: "fas fa-gauge",
+      label: "气压",
+      value: currentWeather.value.now.pressure + "hPa",
+    },
+    {
+      icon: "fas fa-eye",
+      label: "能见度",
+      value: currentWeather.value.now.vis + "km",
+    },
+    {
+      icon: "fas fa-cloud-rain",
+      label: "降水量",
+      value: currentWeather.value.now.precip + "mm",
+    },
+    {
+      icon: "fas fa-cloud",
+      label: "云量",
+      value: currentWeather.value.now.cloud + "%",
+    },
+    {
+      icon: "fas fa-temperature-low",
+      label: "露点温度",
+      value: currentWeather.value.now.dew + "°C",
+    },
+    {
+      icon: "fas fa-thermometer-half",
+      label: "体感温度",
+      value: currentWeather.value.now.feelsLike + "°C",
+    },
+    {
+      icon: "fas fa-temperature-high",
+      label: "实时温度",
+      value: currentWeather.value.now.temp + "°C",
+    },
+  ];
+});
+
+// 实时天气预报：https://dev.qweather.com/docs/api/weather/weather-now/
+const getCurrentWeather = async (id) => {
   try {
     const response = await axios.get(
-      `https://devapi.qweather.com/v7/weather/now?location=${route.query.locationId}&key=${API_KEY}`,
+      `https://devapi.qweather.com/v7/weather/now?location=${id}&key=${API_KEY}`,
     );
-
     return response.data;
   } catch (err) {
     console.log(err);
   }
 };
 
-// 调用getWeatherData函数并等待它完成，然后将结果赋值给weatherData.value
-// 因为getWeatherData是一个异步函数，所以使用await来等待Promise解析
-weatherData.value = await getWeatherData();
+// 每日天气预报：https://dev.qweather.com/docs/api/weather/weather-daily-forecast/
+const getDailyForecast = async (id) => {
+  try {
+    const response = await axios.get(
+      `https://devapi.qweather.com/v7/weather/7d?location=${id}&key=${API_KEY}`,
+    );
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// 每小时天气预报：https://dev.qweather.com/docs/api/weather/weather-hourly-forecast/
+const getHourlyForecast = async (id) => {
+  try {
+    const response = await axios.get(
+      `https://devapi.qweather.com/v7/weather/24h?location=${id}&key=${API_KEY}`,
+    );
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const locationID = route.query.locationId;
+currentWeather.value = await getCurrentWeather(locationID);
+dailyForecast.value = await getDailyForecast(locationID);
+hourlyForecast.value = await getHourlyForecast(locationID);
 </script>
 
 <template>
-  <div v-if="weatherData">
-    <h1>实时天气</h1>
-    <p><strong>更新时间：</strong> {{ weatherData.updateTime }}</p>
-    <p><strong>温度：</strong> {{ weatherData.now.temp }}°C</p>
-    <p><strong>体感温度：</strong> {{ weatherData.now.feelsLike }}°C</p>
-    <p><strong>天气：</strong> {{ weatherData.now.text }}</p>
-    <p><strong>风向：</strong> {{ weatherData.now.windDir }}</p>
-    <p><strong>风速：</strong> {{ weatherData.now.windSpeed }} km/h</p>
-    <p><strong>湿度：</strong> {{ weatherData.now.humidity }}%</p>
-    <p><strong>气压：</strong> {{ weatherData.now.pressure }} hPa</p>
-    <p><strong>能见度：</strong> {{ weatherData.now.vis }} km</p>
-    <a :href="weatherData.fxLink" target="_blank"> 查看更多天气信息 </a>
+  <div class="flex flex-col items-center">
+    <!--Banner-->
+    <div class="w-full bg-weather-secondary p-4 text-center text-white">
+      <p>
+        您当前正在预览该城市，点击
+        <i class="fa-solid fa-plus hover:text-neutral-200"></i>
+        图标开始追踪此城市。
+      </p>
+    </div>
+
+    <!--Weather Overview-->
+    <div class="flex flex-col items-center gap-10 py-12 text-white">
+      <!--City Name & Update Time-->
+      <VStack class="gap-2">
+        <h1 class="text-4xl">{{ route.params.name }}</h1>
+        <p class="text-sm">{{ updateTimeFormatted }}</p>
+      </VStack>
+
+      <!--Weather Icon & Temperature-->
+      <HStack class="gap-4">
+        <i :class="'qi-' + currentWeather.now.icon" class="text-6xl"></i>
+        <p class="text-6xl">{{ Math.round(currentWeather.now.temp) }}°</p>
+      </HStack>
+
+      <!--Weather Description-->
+      <p>
+        天气{{ currentWeather.now.text }}，体感温度约为
+        {{ Math.round(currentWeather.now.feelsLike) }}°C。
+      </p>
+    </div>
+
+    <hr class="w-full border border-white opacity-10" />
+
+    <!--Weather Details-->
+    <div class="my-6 flex flex-wrap justify-center text-white">
+      <weather-detail-card
+        v-for="item in weatherItems"
+        :key="item.label"
+        :iconClass="item.icon"
+        :label="item.label"
+        :value="item.value"
+      />
+
+      <a
+        :href="currentWeather.fxLink"
+        class="my-4 block text-center text-white hover:underline"
+        target="_blank"
+      >
+        查看更多天气信息
+      </a>
+    </div>
+    <div v-if="dailyForecast">
+      <div class="container mx-auto py-4">
+        <h2 class="mb-4 text-2xl font-bold">每日天气预报</h2>
+        <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          <div
+            v-for="day in dailyForecast.daily"
+            :key="day.fxDate"
+            class="rounded-lg border p-4"
+          >
+            <h3 class="text-xl font-bold">
+              {{ day.fxDate | formatDate }}
+            </h3>
+            <div class="mt-2 flex items-center">
+              <i :class="`qi-${day.iconDay}`" class="text-4xl"></i>
+              <div class="ml-4">
+                <p>{{ day.textDay }}</p>
+                <p class="font-bold">{{ day.tempMax }}° / {{ day.tempMin }}°</p>
+              </div>
+            </div>
+            <p class="mt-2"><strong>日出:</strong> {{ day.sunrise }}</p>
+            <p><strong>日落:</strong> {{ day.sunset }}</p>
+            <p><strong>风向:</strong> {{ day.windDirDay }}</p>
+            <p><strong>风速:</strong> {{ day.windSpeedDay }} m/s</p>
+            <p><strong>湿度:</strong> {{ day.humidity }}%</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <div v-else>加载中...</div>
 </template>
---
+++ src/components/WeatherDetailCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  iconClass: String,
+  label: String,
+  value: String,
+});
+</script>
+
+<template>
+  <div
+    class="m-2 flex items-center space-x-2 rounded-lg bg-white bg-opacity-20 p-4"
+  >
+    <i :class="iconClass" class="text-xl"></i>
+    <div>
+      <p class="text-sm">{{ label }}</p>
+      <p class="font-bold">{{ value }}</p>
+    </div>
+  </div>
+</template>
