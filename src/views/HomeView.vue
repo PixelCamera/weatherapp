ef55490e90e99a80edb508c8c970507c96098b05
Chenliang Yan <chenliang.yan@yundx.com>
2023-09-30 15:03:33 +0800

    perf(API): Improved request timer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,22 +7,23 @@ const citySearchResults = ref([]);
 const queryTimeout = ref(null);
 const qweatherApiKey = "7d0835ccc03346ffb8dc8e5525272f98"; // 和风天气API密钥
 const getSearchResults = () => {
-  // 为搜索请求设置定时器并存储，并在下次timeout请求前清除
-  queryTimeout.value = setTimeout(async () => {
+  // 如果存在定时器，清除定时器，防止频繁请求
+  if (queryTimeout.value){
     clearTimeout(queryTimeout.value);
+  }
 
-    // 如果查询条件不为空，则发送请求
+  // 请求设置定时器，存储在queryTimeout中
+  queryTimeout.value = setTimeout(async () => {
     if (citySearchQuery.value !== "") {
       const result = await axios.get(
         `https://geoapi.qweather.com/v2/city/lookup?location=${citySearchQuery.value}&key=${qweatherApiKey}`,
       );
-
-      citySearchResults.value = result.data.location; // 设置搜索结果
-      return; // 结束函数，防止下面的代码执行
+      citySearchResults.value = result.data.location;
+      return;
     }
-    citySearchResults.value = null; // 如果查询条件为空，则清除搜索结果
-  }, 300);
-};
+    citySearchResults.value = null;
+  }, 1000);
+}
 </script>
 <template>
   <div class="container mb-8 pt-4 text-white">
