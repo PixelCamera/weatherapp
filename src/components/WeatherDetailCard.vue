ceb663aa50319e7b224d70b2fd50f2895e4ced3e
Chenliang Yan <chenliang.yan@yundx.com>
2023-10-01 17:41:31 +0800

    perf(WeatherDetailCard): Add hover cursor-pointer

--- src/components/WeatherDetailCard.vue
+++ src/components/WeatherDetailCard.vue
@@ -8,7 +8,7 @@ defineProps({
 
 <template>
   <div
-    class="flex items-center gap-4 rounded-lg bg-white bg-opacity-10 px-4 py-2.5 text-neutral-100 duration-200 hover:-translate-y-0.5 hover:bg-opacity-30 hover:text-white hover:shadow-lg hover:ring-2 hover:ring-neutral-200"
+    class="flex items-center gap-4 rounded-lg bg-white bg-opacity-10 px-4 py-2.5 text-neutral-100 duration-200 hover:-translate-y-0.5 hover:cursor-pointer hover:bg-opacity-30 hover:text-white hover:shadow-lg hover:ring-2 hover:ring-neutral-200"
   >
     <i :class="iconClass" class="text-2xl"></i>
     <div>
