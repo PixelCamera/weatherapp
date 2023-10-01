6d2ab6592c2bda6283e1d3ca05fa5fb9a195f6bc
Chenliang Yan <chenliang.yan@yundx.com>
2023-10-02 02:58:52 +0800

    perf: Optimize performance

---
+++ src/views/CityView/components/HourlyForecastCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+import VStack from "@/components/global/VStack.vue";
+
+defineProps({
+  time: String,
+  iconClass: String,
+  temp: Number,
+  text: String,
+});
+</script>
+
+<template>
+  <v-stack
+    class="w-full gap-1 rounded-lg px-4 py-3 text-neutral-100 duration-200 hover:-translate-y-0.5 hover:cursor-pointer hover:bg-white hover:bg-opacity-20 hover:text-white hover:shadow-2xl hover:ring-2 hover:ring-neutral-200"
+  >
+    <p>{{ time }}</p>
+    <i :class="iconClass" class="text-3xl"></i>
+    <p class="text-lg">{{ temp }}°C</p>
+    <p class="text-sm">{{ text }}</p>
+  </v-stack>
+</template>
---
+++ src/views/CityView/components/WeatherDetailCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HStack from "@/components/global/HStack.vue";
+
+defineProps({
+  iconClass: String,
+  label: String,
+  value: String,
+});
+</script>
+
+<template>
+  <h-stack
+    class="gap-4 rounded-lg bg-white bg-opacity-5 px-5 py-3 text-neutral-100 duration-200 hover:-translate-y-0.5 hover:cursor-pointer hover:bg-opacity-20 hover:text-white hover:shadow-2xl hover:ring-2 hover:ring-neutral-200"
+  >
+    <i :class="iconClass" class="text-2xl"></i>
+    <div>
+      <p class="text-sm">{{ label }}</p>
+      <p class="font-bold">{{ value }}</p>
+    </div>
+  </h-stack>
+</template>
