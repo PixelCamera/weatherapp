76d058f2b9cc8bfe9a2896a6e2887ecca37cd62f
Chenliang Yan <chenliang.yan@yundx.com>
2023-10-02 07:15:22 +0800

    perf(Style): Improve hover effect

--- src/views/CityView/components/HourlyForecastCard.vue
+++ src/views/CityView/components/HourlyForecastCard.vue
@@ -11,7 +11,7 @@ defineProps({
 
 <template>
   <v-stack
-    class="w-full gap-1 rounded-lg px-5 py-3 text-neutral-100 duration-200 hover:-translate-y-0.5 hover:cursor-pointer hover:bg-white hover:bg-opacity-20 hover:text-white hover:shadow-2xl hover:ring-2 hover:ring-neutral-200"
+    class="w-full gap-1 rounded-lg px-5 py-3 text-neutral-100 duration-200 hover:-translate-y-1 hover:scale-110 hover:cursor-pointer hover:bg-white hover:bg-opacity-20 hover:text-white hover:shadow-2xl hover:ring-2 hover:ring-neutral-200"
   >
     <p>{{ time }}</p>
     <i :class="iconClass" class="text-3xl"></i>
--- src/views/CityView/components/WeatherDetailCard.vue
+++ src/views/CityView/components/WeatherDetailCard.vue
@@ -10,7 +10,7 @@ defineProps({
 
 <template>
   <h-stack
-    class="gap-4 rounded-lg bg-white bg-opacity-5 px-5 py-3 text-neutral-100 duration-200 hover:-translate-y-0.5 hover:cursor-pointer hover:bg-opacity-20 hover:text-white hover:shadow-2xl hover:ring-2 hover:ring-neutral-200"
+    class="gap-4 rounded-lg bg-white bg-opacity-5 px-5 py-3 text-neutral-100 duration-200 hover:-translate-y-1 hover:scale-110 hover:cursor-pointer hover:bg-opacity-20 hover:text-white hover:shadow-2xl hover:ring-2 hover:ring-neutral-200"
   >
     <i :class="iconClass" class="text-2xl"></i>
     <div>
