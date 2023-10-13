8f286a4387422c0abb3ea5e5a4eafec61be180dd
Sympathy <abnerpants@gmail.com>
2023-10-13 11:34:02 +0800

    feat(SiteFooter): add SiteFooter component
    Add a new component called SiteFooter.vue to the project. This component includes the copyright section and the powered by section of the website footer. It also imports the HStack component from "@/components/global/HStack.vue" for layout purposes.
    
    This feature enhances the user experience by providing a complete and professional-looking footer for the website.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-import { RouterView } from "vue-router";
+import {RouterView} from "vue-router";
 import SiteNavigation from "@/components/SiteNavigation.vue";
+import SiteFooter from "@/components/SiteFooter.vue";
 </script>
 
 <template>
   <div class="flex min-h-screen flex-col bg-weather-primary font-Roboto">
     <site-navigation />
     <router-view />
+    <site-footer/>
   </div>
 </template>
---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <!--Copyright section-->
+  <footer
+      class="mt-8 flex flex-col items-center justify-center gap-1 bg-black bg-opacity-10 py-6 text-sm text-neutral-200 md:flex-row md:gap-4"
+  >
+    <h-Stack class="gap-1">
+      <span>© 2023 Copyright</span>
+      <a
+          class="font-semibold hover:underline"
+          href="https://github.com/PixelCamera"
+          target="_blank"
+      >PixelCamera</a
+      >
+    </h-Stack>
+    <h-stack class="gap-1">
+      <span>Powered by</span>
+      <a
+          class="font-semibold hover:underline"
+          href="https://www.qweather.com"
+          target="_blank"
+      >QWeather</a
+      >
+    </h-stack>
+  </footer>
+</template>
+<script setup>
+import HStack from "@/components/global/HStack.vue";
+</script>
