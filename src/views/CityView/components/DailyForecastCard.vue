8728e9fda3651d3a97ae36bb15974e3101bfce7f
Sympathy <abnerpants@gmail.com>
2023-10-13 13:01:35 +0800

    refactor(DailyForecastCard): adjust flex layout and remove unnecessary text
    The DailyForecastCard component in the CityView now adjusts the flex layout
    and removes unnecessary text. The date and weekday are now displayed in a flex
    layout with a gap of 1. The text "日出" (sunrise) and "日落" (sunset) are removed
    from the component.

--- src/views/CityView/components/DailyForecastCard.vue
+++ src/views/CityView/components/DailyForecastCard.vue
@@ -17,12 +17,12 @@ defineProps({
 <template>
   <!--Cards-->
   <h-stack
-    class="rounded-lg bg-white bg-opacity-5 p-4 text-neutral-100 duration-200 hover:-translate-y-2 hover:cursor-pointer hover:bg-white hover:bg-opacity-20 hover:text-white hover:shadow-2xl hover:ring-2 hover:ring-neutral-200 lg:flex-col"
+      class="rounded-lg bg-white bg-opacity-5 p-4 text-neutral-100 duration-200 hover:-translate-y-2 hover:cursor-pointer hover:bg-white hover:bg-opacity-20 hover:text-white hover:shadow-2xl hover:ring-2 hover:ring-neutral-200 lg:flex-col"
   >
     <!--Date & Weekday-->
-    <div class="text-md basis-1/4 font-bold">
-      {{ date }}
-      {{ weekday }}
+    <div class="text-md basis-1/4 font-bold flex flex-wrap gap-1 justify-center">
+      <p>{{ date }}</p>
+      <p>{{ weekday }}</p>
     </div>
 
     <!--Weather Icon & Sunrise/Sunset-->
@@ -37,12 +37,10 @@ defineProps({
       <h-stack class="gap-2">
         <h-stack class="gap-1">
           <i class="fas fa-sun text-yellow-400"></i>
-          <p class="text-sm">日出</p>
           <p class="text-sm">{{ sunrise }}</p>
         </h-stack>
         <h-stack class="gap-1">
           <i class="fas fa-moon text-yellow-400"></i>
-          <p class="text-sm">日落</p>
           <p class="text-sm">{{ sunset }}</p>
         </h-stack>
       </h-stack>
