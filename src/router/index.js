7fa5bd05d3289ed36aff354b849ec4e071afe057
Sympathy <abnerpants@gmail.com>
2023-10-19 22:08:07 +0800

    refactor(router): add dynamic page title
    Add dynamic page title based on the route meta data and parameters.
    
    - Set the page title to "首页" for the home route.
    - Set the page title to "城市天气" for the cityView route.
    - Set the page title to "{name}, {adm1}, {country} | WeatherApp" for other routes.
    
    This will provide a more informative and relevant title for each page, enhancing the user experience.

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import CityView from "@/views/CityView/CityView.vue";
 
@@ -9,12 +9,27 @@ let router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: {
+        title: "首页",
+      },
     },
     {
       path: "/weather/:country/:adm1/:name",
       name: "cityView",
       component: CityView,
+      meta: {
+        title: "城市天气",
+      },
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${
+      to.params.country
+          ? `${to.params.name}, ${to.params.adm1}, ${to.params.country}`
+          : to.meta.title
+  }   | WeatherApp`;
+  next();
 });
 export default router;
