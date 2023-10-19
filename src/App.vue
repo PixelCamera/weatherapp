55214b97c978d9fcd7163188d2be3822b658dd6a
Sympathy <abnerpants@gmail.com>
2023-10-19 22:08:29 +0800

    refactor(App): update layout and transitions
    Update the layout and transitions of the App component.
    
    The changes include:
    - Added a transition effect to the router view component.
    - Updated the CSS classes for better styling.
    
    The updated code improves the visual experience of the app and provides smoother transitions between pages.

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,26 @@ import SiteFooter from "@/components/SiteFooter.vue";
 </script>
 
 <template>
-  <div class="flex min-h-screen flex-col bg-weather-primary font-Roboto">
-    <site-navigation />
-    <router-view />
+  <div class="flex min-h-screen w-full flex-col bg-weather-primary font-Roboto">
+    <site-navigation/>
+    <router-view v-slot="{ Component }" class="flex-1">
+      <transition mode="in-out" name="page">
+        <Component :is="Component"/>
+      </transition>
+    </router-view>
+
     <site-footer/>
   </div>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: 600ms ease all;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
